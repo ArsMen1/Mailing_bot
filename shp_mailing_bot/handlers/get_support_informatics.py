a3995de127fb59ed59cc0c5cee614e80a77120e9
ktrntrsv <ktrntrsv@gmail.com>
2022-09-06 17:29:23 +0300

    Supeduapp -> supinformatics

--- shp_mailing_bot/handlers/get_support_informatics.py
+++ shp_mailing_bot/handlers/get_support_informatics.py
@@ -6,5 +6,7 @@ from telegram.ext import CallbackContext
 
 def get_support_informatics(update: Update, context: CallbackContext):
     logger.info(f"[{update.effective_user.name}] got support informatics link")
-    update.message.reply_text(f"@supeduapp — сюда вы можете обратиться, если у вас возникла проблема с Информатикс. "
-                              f"\nПоддержка работает 5/2 с 9:30 до 18:00 ☘️")
+    update.message.reply_text(
+        f"@supinformatics — сюда вы можете обратиться, если у вас возникла проблема с Информатикс."
+        f"\nПоддержка работает 5/2 с 9:30 до 18:00 ☘️"
+    )
