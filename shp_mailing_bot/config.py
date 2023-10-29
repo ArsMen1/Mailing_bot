b3c2f9376bad926898a56173774a86490ed039a6
ktrntrsv <ktrntrsv@gmail.com>
2023-10-29 07:33:20 +0000

    fixed return NPS

--- shp_mailing_bot/config.py
+++ shp_mailing_bot/config.py
@@ -17,7 +17,7 @@ CONSTRUCTOR_SUGGESTIONS_FORM_LINK = "https://forms.gle/5K9kNNCwb7h4fCzg6"
 database_id_history_of_indicators = "0223e0cec25b4509b0b21daf28a01c1c"
 TG_ID_MEAN_INDICATORS = 1000000  # tg id for getting average meanings
 
-semesters_names = ["18/19-I", "18/19-II", "19/20-I", "19/20-II", "20/21-I", "20/21-II", "21/22-I", "21/22-II"]
+semesters_names = ["18/19-I", "18/19-II", "19/20-I", "19/20-II", "20/21-I", "20/21-II", "21/22-I", "21/22-II", "22/23-I", "22/23-II"]
 # change it every semester
 ACTUAL_SEM = semesters_names[len(semesters_names) - 1]
 RESPONSIBLE_FOR_THE_BOT = "@PimenovaYuliya"
